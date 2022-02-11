0437bba676915ab6daf16a8426887e6f3a1f13dc
Toby <newtobycm@gmail.com>
2022-02-10 19:40:30 -0800

    Register slash command done

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,6 +3,7 @@ const fs = require("fs")
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
+const clientId = process.env.CLIENT_ID
 
 const commandFolders = fs.readdirSync('./commands');
 
@@ -11,23 +12,16 @@ var commands = [];
 for (const folder of commandFolders) {
 	const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
 	for (const file of commandFiles) {
-		commandname = file.replace(".js", "")
-		commands.push(commandname);
+		var commandname = file.replace(".js", "")
+		var slashcommand = new SlashCommandBuilder().setName(commandname).setDescription(commandname);
+		commands.push(slashcommand);
 	}
 }
-
-console.log(commands)
 
 commands = commands.map(command => JSON.stringify(command));
 
 const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+rest.put(Routes.applicationCommands(clientId), { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error);
-
-
-
-	// new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-	// new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-	// new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
